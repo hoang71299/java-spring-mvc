5c5e6d23aee185ab4f50e0888387d79003d338fd
Hoang <huynhhoang712003@gmail.com>
2024-10-27 00:24:28 +0700

    Apply MVC model

--- src/main/java/vn/hoidanit/laptopshop/services/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/services/UserService.java
@@ -3,6 +3,8 @@ package vn.hoidanit.laptopshop.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyService {
-
+public class UserService {
+  public String handleHello() {
+    return "Hello from service";
+  }
 }
